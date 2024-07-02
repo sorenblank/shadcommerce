f7ac3f7780267d7435d72b91fd5517ef840fb7b4
sorenblank <afrozaman12@gmail.com>
2024-07-02 06:42:15 +0600

    feat: Added a simple loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+        <>
+        <div className="w-screen h-screen flex justify-center items-center bg-black text-white">
+            Loading...
+        </div>
+        </>
+    )
+}

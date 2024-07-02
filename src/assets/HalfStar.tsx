49b0fee37a5e90b874d39529bdd1f5f792169086
sorenblank <afrozaman12@gmail.com>
2024-07-02 06:36:01 +0600

    feat: Added star assets

---
+++ src/assets/HalfStar.tsx
@@ -0,0 +1,17 @@
+export default function HalfStar() {
+  return (
+    <svg
+      width="9"
+      height="17"
+      viewBox="0 0 9 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        id="Star 5"
+        d="M3.56594 16.9794L8.99998 13.9561V0.255127L6.38077 5.89504L0.20752 6.64322L4.76201 10.8771L3.56594 16.9794Z"
+        fill="#FFC633"
+      />
+    </svg>
+  );
+}
---
+++ src/assets/Star.tsx
@@ -0,0 +1,17 @@
+export default function Star() {
+  return (
+    <svg
+      width="19"
+      height="17"
+      viewBox="0 0 19 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        id="Star 5"
+        d="M9.45246 0.255127L12.0717 5.89504L18.2449 6.64322L13.6904 10.8771L14.8865 16.9794L9.45246 13.9561L4.01842 16.9794L5.21449 10.8771L0.659999 6.64322L6.83325 5.89504L9.45246 0.255127Z"
+        fill="#FFC633"
+      />
+    </svg>
+  );
+}
